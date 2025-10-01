db64ca9281bba37f95d7df021dd790139bad2752
Sachin <devsachinmaurya241@gmail.com>
2025-10-01 17:47:18 +0530

    helloworld program updated

--- Stage0/HelloWorld.java
+++ Stage0/HelloWorld.java
@@ -1,20 +1,27 @@
+// Stage 0 - Day 1 Assignment
+// Simple Hello World + Command-line arguments
+
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, Sachin - Your Java Journey!");
-        int sum=0,pro=1;
-        if(args.length>0)
-        {
-            for (String arg : args) {
-                sum += Integer.parseInt(arg);
-                pro *= Integer.parseInt(arg);
-            }
-            System.out.println("Sum ="+sum);
-            System.out.println("Product ="+pro);
-            if (Integer.parseInt(args[0])>Integer.parseInt(args[1])) {
-                System.out.println("1st Integer is Greater");
-            } else {
-                System.out.println("2nd Integer is Greater");
-            }
+        // Print welcome message
+        System.out.println("Hello Sachin, welcome to your Java Journey!");
+
+        // Validate arguments
+        if (args.length < 2) {
+            System.out.println("Please provide two numbers as arguments.");
+            return;
         }
+
+        // Parse command-line arguments
+        int num1 = Integer.parseInt(args[0]);
+        int num2 = Integer.parseInt(args[1]);
+
+        // Display numbers
+        System.out.println("First number: " + num1);
+        System.out.println("Second number: " + num2);
+
+        // Example calculation
+        int sum = num1 + num2;
+        System.out.println("Sum: " + sum);
     }
 }
